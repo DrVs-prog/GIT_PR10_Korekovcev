d2757af4243c26ae8e5e3e0fd729050f42b59f5a
МОСЯГИН АНТОН <anton.mosyagin.is22b@gmail.com>
2026-01-28 11:16:16 +0300

    Task 4: Add group number and full name

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main()
     {
         Console.WriteLine($"Текущая дата: {DateTime.Now.ToShortDateString()}");
+        Console.WriteLine($"Группа: ИС-22Б");
+        Console.WriteLine($"Студент: Корековцев Дмитрий Андреевич");
     }
 }
